b558327de504439f20180b34996773d1af2f3911
cooploop2663 <93048858+cooploop2663@users.noreply.github.com>
2024-10-03 10:40:02 -0400

    Update dnsserv.py

--- dnsserv.py
+++ dnsserv.py
@@ -1,39 +1,38 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import socket
-
 import re
-
 import binascii
-
 from dnslib import DNSRecord
 
-UDP_IP = '0.0.0.0'
+# sudo python3 dnsserv.py
 
+# IP and Port to listen on
+UDP_IP = "0.0.0.0"
 UDP_PORT = 53
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)  # Internet
-                                                         # UDP
-
+# Create a UDP socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 
+# Listen for incoming DNS requests
 while True:
+    # Receive data from the socket
+    byteData, addr = sock.recvfrom(2048)  # buffer size is 2048 bytes
+    try:
+        # Convert byte data to a hex string and then back to bytes
+        msg = binascii.unhexlify(binascii.b2a_hex(byteData))
+        
+        # Parse the DNS message
+        msg = DNSRecord.parse(msg)
+    except Exception as e:
+        # Print any errors during message processing
+        print(e)
+        continue
 
-    (byteData, addr) = sock.recvfrom(2048)  # buffer size is 2048 bytes
-
-try:
-
-    msg = binascii.unhexlify(binascii.b2a_hex(byteData))
-
-    msg = DNSRecord.parse(msg)
-except Exception, e:
-
-    print e
-
-    continue
-
-m = re.search(r'\;(\S+)\.c2\.cooploop2663\.com', str(msg), re.MULTILINE)
-
-if m:
-
-    print ('got data:', m.group(1))
+    # Regex search for the specific domain pattern in the DNS message
+    m = re.search(r';(\S+)\.sub\.exfi\.tk', str(msg), re.MULTILINE)
+    
+    # If a match is found, print the extracted data
+    if m:
+        print('got data:', m.group(1))
